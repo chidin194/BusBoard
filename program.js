43a5c14577fc1e52aa3467e8a589e5330acbcb22
chidin194 <chiara.dinan@softwire.com>
2022-04-27 12:29:57 +0100

    BusBoard: Refactor getLiveArrivalPredictions function to use a promise instead of nested callbacks

--- program.js
+++ program.js
@@ -6,11 +6,13 @@ const {validatePostcode, getBusStopsInfo} = require('./postcodeApiClient.js');
 const prompt = ps({});
 
 const runProgram = () => {
-    // const userStopCode = prompt('Please enter the stop code of your nearest bus stop: ')
+    const userStopCode = prompt('Please enter the stop code of your nearest bus stop: ')
 
-    const userPostcode = prompt("Please enter your postcode: ");
+    // const userPostcode = prompt("Please enter your postcode: ");
+    //
+    // getBusStopsInfo(userPostcode);
 
-    getBusStopsInfo(userPostcode);
+    getLiveArrivalPredictions(userStopCode);
 }
 
 runProgram()
--- tflApiClient.js
+++ tflApiClient.js
@@ -1,21 +1,43 @@
 const request = require("request");
 
 const getLiveArrivalPredictions = (userStopCode) => {
-    request(`https://api.tfl.gov.uk/StopPoint/${userStopCode}/Arrivals`, function (error, response, body) {
+    // request(`https://api.tfl.gov.uk/StopPoint/${userStopCode}/Arrivals`, function (error, response, body) {
+    //
+    //     const sortedArrivals = JSON.parse(body).sort((b1, b2) => b1.timeToStation - b2.timeToStation);
+    //
+    //     console.log(`Bus stop: ${sortedArrivals[0].stationName}`);
+    //
+    //     sortedArrivals.map(a => {
+    //         if(sortedArrivals.indexOf(a) <= 4) {
+    //             let arrivalTime = Math.floor(a.timeToStation/60);
+    //             console.log(`${a.lineId} to ${a.destinationName} ${arrivalTime <= 0 ? 'due' : 'arriving in ' + arrivalTime + ' minutes'}`)
+    //         }
+    //
+    //     })
+    // });
+    const promise = new Promise((resolve, reject) => {
+            request(`https://api.tfl.gov.uk/StopPoint/${userStopCode}/Arrivals`, function (error, response, body) {
+                const sortedArrivals = JSON.parse(body).sort((b1, b2) => b1.timeToStation - b2.timeToStation);
+                if (sortedArrivals) {
+                    resolve(sortedArrivals)
+                } else {
+                    reject(Error("Unable to fetch data"))
+                }
+            })
+        }
+    ).then(sortedArrivals => {
+        
+            console.log(`Bus stop: ${sortedArrivals[0].stationName}`);
 
-        const sortedArrivals = JSON.parse(body).sort((b1, b2) => b1.timeToStation - b2.timeToStation);
-
-        console.log(`Bus stop: ${sortedArrivals[0].stationName}`);
-
-        sortedArrivals.map(a => {
-            if(sortedArrivals.indexOf(a) <= 4) {
-                let arrivalTime = Math.floor(a.timeToStation/60);
-                console.log(`${a.lineId} to ${a.destinationName} ${arrivalTime <= 0 ? 'due' : 'arriving in ' + arrivalTime + ' minutes'}`)
-            }
-
-        })
-    });
+            sortedArrivals.map(a => {
+                if(sortedArrivals.indexOf(a) <= 4) {
+                    let arrivalTime = Math.floor(a.timeToStation/60);
+                    console.log(`${a.lineId} to ${a.destinationName} ${arrivalTime <= 0 ? 'due' : 'arriving in ' + arrivalTime + ' minutes'}`)
+                }
+            })
+    } )
 }
+
 module.exports = {getLiveArrivalPredictions};
 
 // 490008660N
